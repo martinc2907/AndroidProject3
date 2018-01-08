515a76822daeff2f79a4ee05a3a254a52c588e64
unknown <martinc2907@gmail.com>
2018-01-08 22:04:34 +0900

    second commit:basics

--- app/src/main/java/com/example/user/androidlock/LockScreenActivity.java
+++ app/src/main/java/com/example/user/androidlock/LockScreenActivity.java
@@ -1,7 +1,18 @@
 package com.example.user.androidlock;
 
+import android.app.ActivityManager;
+import android.app.AlertDialog;
+import android.app.KeyguardManager;
+import android.content.Context;
+import android.content.DialogInterface;
 import android.content.Intent;
+import android.graphics.Color;
+import android.net.Uri;
+import android.os.Build;
 import android.os.CountDownTimer;
+import android.os.Handler;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.support.design.widget.FloatingActionButton;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -11,20 +22,37 @@ import android.view.View;
 import android.view.Window;
 import android.view.WindowManager;
 import android.widget.Button;
+import android.widget.ImageView;
+import android.widget.LinearLayout;
 import android.widget.TextView;
 import android.widget.Toast;
 
-public class LockScreenActivity extends AppCompatActivity {
+public class LockScreenActivity extends AppCompatActivity{
     FloatingActionButton unlockBtn;
     TextView ticktock;
+    private HomeKeyLocker mHomeKeyLocker;
+    private AlertDialog.Builder builder;
+    private AlertDialog popup;
+    private CountDownTimer countDownTimer;
+    boolean success = true;
+
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         getSupportActionBar().hide();
 
         //requestWindowFeature(Window.FEATURE_NO_TITLE);
-        //getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
+        getWindow().setFlags(WindowManager.LayoutParams.FLAG_FULLSCREEN, WindowManager.LayoutParams.FLAG_FULLSCREEN);
         setContentView(R.layout.lockscreen);
+
+        mHomeKeyLocker = new HomeKeyLocker();
+        mHomeKeyLocker.lock(this);
+
+        if(!Settings.canDrawOverlays(getApplicationContext())){
+            startActivity(new Intent(Settings.ACTION_MANAGE_OVERLAY_PERMISSION));
+        }
+
 
         ticktock = findViewById(R.id.ticktock);
         unlockBtn = findViewById(R.id.unlock_btn);
@@ -34,13 +62,33 @@ public class LockScreenActivity extends AppCompatActivity {
             public void onClick(View v){
                 getWindow().clearFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
 
-                Toast.makeText(getApplicationContext(),"You gave up!", Toast.LENGTH_LONG).show();
-                //Show some destruction animation.
+                builder = new AlertDialog.Builder(LockScreenActivity.this);
+                View mView = getLayoutInflater().inflate(R.layout.custom_alert,null);
+                Button yesButton = mView.findViewById(R.id.yes_btn);
+                Button noButton = mView.findViewById(R.id.no_btn);
+                builder.setView(mView);
+                popup = builder.create();
+                popup.show();
+
+                yesButton.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        success = false;
+                        countDownTimer.onFinish();
+                    }
+                });
+
+                noButton.setOnClickListener(new View.OnClickListener(){
+                    @Override
+                    public void onClick(View view){
+                        popup.cancel();
+                    }
+                });
             }
         });
 
         int seconds = getIntent().getIntExtra("TIME",10);
-        CountDownTimer countDownTimer = new CountDownTimer(seconds * 1000, 1000) {
+        countDownTimer = new CountDownTimer(seconds * 1000, 1000) {
             @Override
             public void onTick(long millis) {
                 ticktock.setText( Integer.toString((int) (millis / 1000)));
@@ -48,11 +96,58 @@ public class LockScreenActivity extends AppCompatActivity {
 
             @Override
             public void onFinish() {
-                ticktock.setText("Finished");
+
+                this.cancel();
+                if(popup!=null)
+                    popup.cancel();
+
+                //Show success/failure image
+                LinearLayout ll = findViewById(R.id.back);
+                if(success){
+                    ll.setBackgroundResource(R.drawable.test1);
+                    ticktock.setText("Nice!");
+                }else{
+                    ll.setBackgroundResource(R.drawable.test2);
+                    ticktock.setText("Try again next time!");
+                }
+
+                //Go back to previous screen
+                mHomeKeyLocker.unlock();
+                Intent i= new Intent(LockScreenActivity.this, MainActivity.class);
+                //i.setFlags(Intent.FLAG_ACTIVITY_REORDER_TO_FRONT);
+                startActivity(i);
+                overridePendingTransition(R.anim.fade_in_long,R.anim.fade_out_long);
+                success = true;
             }
         }.start();
 
         getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED | WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
     }
+
+    @Override
+    protected void onPause(){
+        super.onPause();
+
+        ActivityManager activityManager = (ActivityManager) getApplicationContext()
+                .getSystemService(Context.ACTIVITY_SERVICE);
+
+        activityManager.moveTaskToFront(getTaskId(), 0);
+    }
+
+    /*
+    @Override
+    public void onBackPressed(){
+        return;
+    }
+*/
+    /*
+    @Override
+    public void onAttachedToWindow() {
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_SHOW_WHEN_LOCKED);
+        getWindow().addFlags(WindowManager.LayoutParams.FLAG_DISMISS_KEYGUARD);
+    }
+
+*/
+
 
 }
--- app/src/main/java/com/example/user/androidlock/MainActivity.java
+++ app/src/main/java/com/example/user/androidlock/MainActivity.java
@@ -1,30 +1,29 @@
 package com.example.user.androidlock;
 
 import android.app.Activity;
-import android.app.AlarmManager;
-import android.app.PendingIntent;
 import android.content.Intent;
+import android.os.Build;
 import android.os.Bundle;
-import android.os.CountDownTimer;
+import android.provider.Settings;
+import android.support.annotation.RequiresApi;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
-import android.widget.TextView;
-import com.example.user.androidlock.R;
 
 /**
  * Created by user on 2018-01-06.
  */
 
-public class ConfigActivity extends Activity {
+public class MainActivity extends Activity {
 
     private Button lock_btn;
     private EditText timer;
 
+    @RequiresApi(api = Build.VERSION_CODES.M)
     @Override
     protected void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.asdf);
+        setContentView(R.layout.main);
 
         lock_btn = findViewById(R.id.lock_btn);
         timer = findViewById(R.id.timer_input);
@@ -33,11 +32,12 @@ public class ConfigActivity extends Activity {
             @Override
             public void onClick(View v){
                 String text = timer.getText().toString();
+                timer.setText("");
                 int seconds = Integer.valueOf(text);
-                Intent intent = new Intent(ConfigActivity.this, LockScreenActivity.class);
+                Intent intent = new Intent(MainActivity.this, LockScreenActivity.class);
                 intent.putExtra("TIME", seconds);
                 startActivity(intent);
-
+                overridePendingTransition(R.anim.fade_in,R.anim.fade_out);
             }
         });
     }
